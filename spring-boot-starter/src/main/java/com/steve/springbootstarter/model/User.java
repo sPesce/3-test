a22032a2af7255c85379191bef952aa163f0bf09
Steve Pesce <stevepesce879@gmail.com>
2020-10-21 17:32:36 -0400

    first commit

---
+++ spring-boot-starter/src/main/java/com/steve/springbootstarter/model/User.java
@@ -0,0 +1,65 @@
+package com.steve.springbootstarter.model;
+
+import java.util.UUID;
+
+public class User {
+
+  //user id
+  private final UUID userUid;
+  private final String firstName;
+  private final String lastName;
+  private final Gender gender;
+  private final Integer age;
+  private final String email;
+
+  public User(UUID userUid, String firstName, String lastName, Gender gender, Integer age, String email) {
+    this.userUid = userUid;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.age = age;
+    this.email = email;
+  }
+
+  public UUID getUserUid() {
+    return this.userUid;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public Gender getGender() {
+    return this.gender;
+  }
+
+  public Integer getAge() {
+    return this.age;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+      " userUid='" + getUserUid() + "'" +
+      ", firstName='" + getFirstName() + "'" +
+      ", lastName='" + getLastName() + "'" +
+      ", gender='" + getGender() + "'" +
+      ", age='" + getAge() + "'" +
+      ", email='" + getEmail() + "'" +
+      "}";
+  }
+
+  enum Gender{
+    MALE,
+    FEMALE
+  }
+  
+}
---
+++ spring-boot-starter/src/test/java/com/steve/springbootstarter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
