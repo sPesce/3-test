8c8ba632c7f593e217500d46f124b2be8f4a5a3e
Steve Pesce <stevepesce879@gmail.com>
2020-10-28 15:47:08 -0400

    finished fetch user(s), created error msg

--- spring-boot-starter/src/main/java/com/steve/springbootstarter/resource/UserResource.java
+++ spring-boot-starter/src/main/java/com/steve/springbootstarter/resource/UserResource.java
@@ -1,18 +1,23 @@
 package com.steve.springbootstarter.resource;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.steve.springbootstarter.model.User;
 import com.steve.springbootstarter.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(
-  path = "/api/v1/users"
+  path = "/api/v1/users"//  <<-----------------------------------
 )
 public class UserResource {
   
@@ -31,5 +36,43 @@ public class UserResource {
     return userService.getAllUsers();
   }
 
+  @RequestMapping(
+    method = RequestMethod.GET,
+    path = "{userUid}"
+  )
+  public ResponseEntity<?> fetchUser(@PathVariable("userUid") UUID userUid) {
+    Optional<User> userOptional = userService.getUser(userUid);
+    if( userOptional.isPresent())
+      return ResponseEntity.ok(userOptional.get());
+    else
+      return (
+        ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(new ErrorMessage("user " + userUid + " was not found."))
+      );
+  }
+  private class ErrorMessage {
+    private String message;
   
+    public ErrorMessage(String message) {
+      this.message = message;
+    }
+  
+    public String getMessage() {
+      return this.message;
+    }
+  
+    public void setMessage(String message) {
+      this.message = message;
+    }
+  
+    @Override
+    public String toString() {
+      return "{" +
+        " message='" + message + "'" +
+        "}";
+    }
+  }
+
+
 }
