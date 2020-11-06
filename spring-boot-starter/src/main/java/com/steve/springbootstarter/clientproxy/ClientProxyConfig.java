63bece5a8944aaa3441d89b0748c793445c65fb7
Steve Pesce <stevepesce879@gmail.com>
2020-11-05 19:23:12 -0500

    fixed bad version reclientproxy;

--- spring-boot-starter/src/main/java/com/steve/springbootstarter/clientproxy/ClientProxyConfig.java
+++ spring-boot-starter/src/main/java/com/steve/springbootstarter/clientproxy/ClientProxyConfig.java
@@ -1,20 +1,23 @@
 package com.steve.springbootstarter.clientproxy;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientProxyConfig {
   
+  @Value("${users.api.url.v1}")
+  private String USERS_URL;
+
   @Bean
   public UserResourceV1 getUserResourceV1() {
-    ResteasyClient client = new ResteasyClientBuilder().build();
+    ResteasyClient client = new ResteasyClientBuilderImpl().build();
 
-    final String usersEndpointUrl = "http://localhost:8080/api/v1/users";
-    ResteasyWebTarget target = client.target(usersEndpointUrl);
+    ResteasyWebTarget target = client.target(USERS_URL);
 
     return target.proxy(UserResourceV1.class);
   }
